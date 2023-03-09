0efe41e01c235421358705e8a2100b3907053004
ni_cats <Ni_cats@163.com>
2023-03-09 10:49:00 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,17 @@
+package com.atguigu.git;
+
+/**
+ * @BelongsProject: Git-test
+ * @BelongsPackage: com.atguigu.git
+ * @Author: Ni_cats
+ * @email: Ni_cats@163.com
+ * @CreateTime: 2023-03-09  10:41
+ * @Description: TODO
+ * @Version: 1.0
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
