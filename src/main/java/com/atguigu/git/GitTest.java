92eb998f4afd680d736819966e705bbe61bb0c54
ni_cats <Ni_cats@163.com>
2023-03-09 11:07:32 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,7 +14,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello github!");
-        System.out.println("hello jiang!");
+        System.out.println("hello jiangminjie!");
         System.out.println("hello zheng!");
     }
 }
